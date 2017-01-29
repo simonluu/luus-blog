dec31e21842c357940bab6e6b961214533e13c86
Simon Luu <luus2016@gmail.com>
2017-01-28 17:21:03 -0800

    updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,26 @@
-var webpack = require('webpack');
-
 module.exports = {
-  entry: './src/index.js',
-  output: { path: __dirname, publicPath: '/', filename: 'bundle.js' },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'stage-1', 'react']
-        }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
+  entry: [
+    './src/index.js'
   ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
 };
