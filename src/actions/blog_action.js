177e4d3407a99724844b7bb87132f7b0973f2fd5
Simon Luu <luus2016@gmail.com>
2017-01-28 17:46:09 -0800

    fixed production build

--- src/actions/blog_action.js
+++ src/actions/blog_action.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '?key=XMP93LtWUFRRrq2Iqr0D';
 
-const ROOT_URL = `http://reduxblog.herokuapp.com/api`;
+const ROOT_URL = `https://reduxblog.herokuapp.com/api`;
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -19,6 +21,14 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
   devServer: {
     historyApiFallback: true,
     contentBase: './'
